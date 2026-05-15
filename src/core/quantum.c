6fd857ce5d2991514bd17c73db6264c6b67bb47f
MrElizeus <156989051+MrElizeus@users.noreply.github.com>
2026-05-14 23:53:29 -0500

    Keep quantum core focused on units and box formulas

--- src/core/quantum.c
+++ src/core/quantum.c
@@ -10,23 +10,6 @@ double q_j_to_ev(double j)
     return j / Q_E;
 }
 
-double q_photon_energy_j_from_nm(double lambda_nm)
-{
-    double lambda_m = lambda_nm * 1.0e-9;
-    return (Q_H * Q_C) / lambda_m;
-}
-
-double q_photon_energy_ev_from_nm(double lambda_nm)
-{
-    return q_j_to_ev(q_photon_energy_j_from_nm(lambda_nm));
-}
-
-double q_photon_frequency_hz_from_nm(double lambda_nm)
-{
-    double lambda_m = lambda_nm * 1.0e-9;
-    return Q_C / lambda_m;
-}
-
 double q_particle_box_energy_j(int n, double mass_kg, double length_m)
 {
     double nd = (double)n;
